e8edf0f17e5fa548f6c5d6d96131a57192dfa8e7
snadi043 <snadi043@uottawa.ca>
2025-05-12 12:34:33 -0400

    Constructed the route in the fetch method in-order to get the post individually based on the post id.

--- Frontend/src/pages/Feed/SinglePost/SinglePost.js
+++ Frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -17,8 +17,8 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    // The url here has to have the filter for individual feed which is similar to 'http://localhost:8080/feeds/posts/:postId'
-    fetch('http://localhost:8080/feeds/posts/' + postId)
+    // The url here has to have the filter for individual feed which is similar to 'http://localhost:8080/feeds/post/:postId'
+    fetch('http://localhost:8080/feeds/post/' + postId)
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch status');
@@ -29,6 +29,7 @@ class SinglePost extends Component {
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
+          image: 'http://localhost:8080/' + resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content
         });
