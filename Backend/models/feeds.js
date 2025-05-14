c30633edcde95b9f29203b11c7e7332d6c73fb94
snadi043 <snadi043@uottawa.ca>
2025-05-14 18:34:48 -0400

    Created the user model to implement authentication in the application.

--- Backend/models/feeds.js
+++ Backend/models/feeds.js
@@ -5,10 +5,6 @@ const mongoose =  require('mongoose');
 const Schema = mongoose.Schema;
 
 const feedsSchema = new Schema({
-        // _id: {
-        //     type: String,
-        //     required: true,
-        // },
         title: {
             type: String,
             required: true,
---
+++ Backend/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'feeds'
+        }
+    ],
+});
+
+module.exports = mongoose.model('user', userSchema);
