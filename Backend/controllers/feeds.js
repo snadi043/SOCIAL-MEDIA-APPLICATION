2c7ca2d887abde6bceaba0403672fa4967b5830e
snadi043 <snadi043@uottawa.ca>
2025-05-22 15:22:05 -0400

    Refactored the remaining controllers for edit, delete and getPosts action to configure it with the socket.io mechanism to render the changes in all the users browser without refreshing their pages. Also, inserted the missing info into the posts from the socket.io object to render the user name on the browser by using the populate() method and also sorting the posts using the sort() method based on createdAt filter.

--- Backend/controllers/feeds.js
+++ Backend/controllers/feeds.js
@@ -21,6 +21,8 @@ exports.getPosts = async (req, res, next) => {
     try{
         const totalItems = await Feeds.find().countDocuments();
         const posts = await Feeds.find()
+            .populate('creator')
+            .sort({createdAt: -1})
             .skip((pageCount - 1) * perPage)
             .limit(perPage);
             res.status(200).json({
@@ -96,7 +98,11 @@ exports.createPosts = async (req, res, next) => {
         // Configuring the module from socket.js file to create an object which defines the action and the data properties to send it to the client-side. 
         // emit() -> is the method provided by the socket.io to send the notifications or data all the users including the user who created it.
         // broadcast() -> is another method by socket.io which is similar to emit by it doesnot count the user who created it.
-        io.getIO().emit('posts', {action: 'create', post: post});
+        io.getIO().emit('posts', 
+            {
+                action: 'create', 
+                post: { ...post.doc, creator: {_id: req.userId, name: user.name}}
+            });
         res.status(201).json({
             message: 'A new post is created',
             post: post,
@@ -112,58 +118,58 @@ exports.createPosts = async (req, res, next) => {
 }
 
 // Controller to respond to the PUT -> /feeds/post/:postId url in the applicaton.
-exports.editPost = (req, res, next) => {
+exports.editPost = async (req, res, next) => {
     const postId = req.params.postId;
-    // Defining the error handler to trigger the error when it is unable to process the URL.
-    const errors = validationResult(req);
-    if(!errors.isEmpty){
-        const error = new Error('Unable to find the post to edit');
-        error.statusCode = 422;
-        throw error;
-    }
-    const title = req.body.title;
-    const content = req.body.content;
-    let imageUrl = req.body.image; // In the formData the key is set as image in order to fetch the value from the body of the request.
-    if(req.file){
-        imageUrl = req.file.path;
-    }
-    if(!imageUrl){
-        const error = new Error('Unable to find the post to edit');
-        error.statusCode = 422;
-        throw error;
-    }
-    Feeds.findById(postId).then(post => {
-        if(!post){
-            const error = new Error('Unable to find the post with an ' + postId);
-            error.statusCode = 404;
+    try{
+        // Defining the error handler to trigger the error when it is unable to process the URL.
+        const errors = validationResult(req);
+        if(!errors.isEmpty){
+            const error = new Error('Unable to find the post to edit');
+            error.statusCode = 422;
             throw error;
         }
-        if(post.creator.toString() !== req.userId){
-            const error = new Error('User access not allowed. Unable to edit the post');
-            error.statusCode = 403;
+        const title = req.body.title;
+        const content = req.body.content;
+        let imageUrl = req.body.image; // In the formData the key is set as image in order to fetch the value from the body of the request.
+        if(req.file){
+            imageUrl = req.file.path;
+        }
+        if(!imageUrl){
+            const error = new Error('Unable to find the post to edit');
+            error.statusCode = 422;
             throw error;
         }
-        if(imageUrl !== post.imageUrl){
-            deleteImage(post.imageUrl);
-        }
-        post.title = title;
-        post.imageUrl = imageUrl; // imageUrl is the key in the model for the Feeds.
-        post.content = content;
-        return post.save();
-    })
-    .then(result => {
-        res.status(200).json({
-            message: 'Post edited successfully',
-            post: result
-        });
-    })
-    .catch(err => {
+        const post = await Feeds.findById(postId).populate('creator');
+            if(!post){
+                const error = new Error('Unable to find the post with an ' + postId);
+                error.statusCode = 404;
+                throw error;
+            }
+            if(post.creator._id.toString() !== req.userId){
+                const error = new Error('User access not allowed. Unable to edit the post');
+                error.statusCode = 403;
+                throw error;
+            }
+            if(imageUrl !== post.imageUrl){
+                deleteImage(post.imageUrl);
+            }
+            post.title = title;
+            post.imageUrl = imageUrl; // imageUrl is the key in the model for the Feeds.
+            post.content = content;
+            const result = await post.save();
+            io.getIO().emit('posts', {action: 'update', post: result});
+            res.status(200).json({
+                message: 'Post edited successfully',
+                post: result
+            })
+    }
+    catch(err){
     // Setting the error code, for any other uncatched error set those type of errors to server related error with statusCode of 500.
         if(!err.statusCode){
             err.statusCode = 500;
         }
         next(err);
-    });
+    };
 }
 
 // This is a helper function to clear the images in the application used while updating the post or deleting the post.
@@ -173,10 +179,10 @@ const deleteImage = (filePath) => {
 };
 
 // Controller to respond to the GET -> /feeds/post url in the applicaton.
-exports.deletePost = (req, res, next) => {
+exports.deletePost = async (req, res, next) => {
     const postId = req.params.postId;
-    Feeds.findById(postId)
-    .then(post => {
+    try{
+        const post = await Feeds.findById(postId);
         if(!post){
             const error = new Error('Unable to find the post to delete');
             error.statusCode = 422;
@@ -187,19 +193,19 @@ exports.deletePost = (req, res, next) => {
             error.statusCode = 403;
             throw error;
         }
-            deleteImage(post.imageUrl);
-            return Feeds.findByIdAndDelete(postId);
-    })
-    .then(result => {
-        console.log(result);
-        res.status(200).json({
-            message: 'Post deleted suceessfully',
-        });
-    })
-    .catch(err => {
+        deleteImage(post.imageUrl);
+           await Feeds.findByIdAndDelete(postId);
+           const user = await User.findById(req.userId);
+           await user.save();
+           io.getIO().emit('posts', {action: 'delete', post: postId});
+            res.status(200).json({
+                message: 'Post deleted suceessfully',
+            });   
+    }
+    catch(err){
         if(!err.statusCode){
             err.statusCode = 500;
         }
         next(err);
-    })
+    }
 }
