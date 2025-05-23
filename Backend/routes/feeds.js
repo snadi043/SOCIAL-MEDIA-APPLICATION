de8c707e083676e69f0f0ca0ac971dff5d82ae7a
snadi043 <snadi043@uottawa.ca>
2025-05-23 12:51:30 -0400

    Commented the route files in the application in order to implement the qrapgql mechanism in the application.

--- Backend/routes/feeds.js
+++ Backend/routes/feeds.js
@@ -1,49 +1,53 @@
-// Initializing the install express package to use it in the application.
-const express = require('express');
+// // Initializing the install express package to use it in the application.
+// const express = require('express');
 
-// Importing the controller file to use it in the application to perform the logic/action for the specific routes.
-const feedsController = require('../controllers/feeds');
+// // Importing the controller file to use it in the application to perform the logic/action for the specific routes.
+// const feedsController = require('../controllers/feeds');
 
-// Initializing the express-validator package to configure and utilize it in the application.
-const {body} = require('express-validator'); 
+// // Initializing the express-validator package to configure and utilize it in the application.
+// const {body} = require('express-validator'); 
 
-const isAuth = require('../middleware/is-auth');
+// const isAuth = require('../middleware/is-auth');
 
-// Initializing the router which is provided by the node-express library to use it in the application.
-const router = express.Router();
+// // Initializing the router which is provided by the node-express library to use it in the application.
+// const router = express.Router();
 
-// GET -> /feeds/post
-router.get('/posts', feedsController.getPosts);
+// // GET -> /feeds/post
+// router.get('/posts', feedsController.getPosts);
 
-// GET -> /feeds/post/:postId
-// This is the route to render a single post/feed in the application.
-router.get('/post/:postId', isAuth, feedsController.getPost);
+// // GET -> /feeds/post/:postId
+// // This is the route to render a single post/feed in the application.
+// router.get('/post/:postId', isAuth, feedsController.getPost);
 
-// POST -> /feeds/post
-// Configuring the "POST" route to sanatize and maintain consistentency in the data which is to be accepted in the application with set of error validation filters.
-router.post('/post', 
-    isAuth,  
-    [
-        body('title').isString().isLength({min: 5}).trim().withMessage('Please enter a title with at least 5 charecters'),
-        body('content').isString().isLength({min: 5}).trim().withMessage('Please eneter the content with at least 5 charecters'),
-    ],
-    feedsController.createPosts);
+// // POST -> /feeds/post
+// // Configuring the "POST" route to sanatize and maintain consistentency in the data which is to be accepted in the application with set of error validation filters.
+// router.post('/post', 
+//     isAuth,  
+//     [
+//         body('title').isString().isLength({min: 5}).trim().withMessage('Please enter a title with at least 5 charecters'),
+//         body('content').isString().isLength({min: 5}).trim().withMessage('Please eneter the content with at least 5 charecters'),
+//     ],
+//     feedsController.createPosts);
 
-// PUT -> /feeds/post/:postId
-// Configuring the "PUT" method which is accepted by the javascript  fetch API to update a document and also to sanatize and maintain consistentency in the data 
-// which is to be accepted in the application with set of error validation filters.
-router.put('/post/:postId',
-    isAuth,
-    [
-        body('title').isString().isLength({min: 5}).trim().withMessage('Please enter a title with at least 5 charecters'),
-        body('content').isString().isLength({min: 5}).trim().withMessage('Please eneter the content with at least 5 charecters'),
-    ], 
-    feedsController.editPost);
+// // PUT -> /feeds/post/:postId
+// // Configuring the "PUT" method which is accepted by the javascript  fetch API to update a document and also to sanatize and maintain consistentency in the data 
+// // which is to be accepted in the application with set of error validation filters.
+// router.put('/post/:postId',
+//     isAuth,
+//     [
+//         body('title').isString().isLength({min: 5}).trim().withMessage('Please enter a title with at least 5 charecters'),
+//         body('content').isString().isLength({min: 5}).trim().withMessage('Please eneter the content with at least 5 charecters'),
+//     ], 
+//     feedsController.editPost);
 
-// DELETE -> /feeds/post/:postId
-// Configuring the "DELETE" method which is accepted by the javascript  fetch API to update a document and also to sanatize and maintain consistentency in the data 
-// which is to be accepted in the application with set of error validation filters.
-router.delete('/post/:postId', isAuth, feedsController.deletePost);
+// // DELETE -> /feeds/post/:postId
+// // Configuring the "DELETE" method which is accepted by the javascript  fetch API to update a document and also to sanatize and maintain consistentency in the data 
+// // which is to be accepted in the application with set of error validation filters.
+// router.delete('/post/:postId', isAuth, feedsController.deletePost);
 
-// exporting the module to use it in the application as a centralized router.
-module.exports = router;
+// // exporting the module to use it in the application as a centralized router.
+// module.exports = router;
+
+
+
+                        // COMMENTED THIS FILE TO IMPLEMENT GRAPHQL //
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,36 +1,40 @@
-const express = require('express');
-const { body } = require('express-validator');
+// const express = require('express');
+// const { body } = require('express-validator');
 
-const User = require('../models/user');
-const userController = require('../controllers/user');
+// const User = require('../models/user');
+// const userController = require('../controllers/user');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.put(
-  '/signup',
-  [
-    body('email')
-      .isEmail()
-      .withMessage('Please enter a valid email.')
-      .custom((value, { req }) => {
-        return User.findOne({ email: value }).then(userDoc => {
-          if (userDoc) {
-            return Promise.reject('E-Mail address already exists!');
-          }
-        });
-      })
-      .normalizeEmail(),
-    body('password')
-      .trim()
-      .isLength({ min: 5 }),
-    body('name')
-      .trim()
-      .not()
-      .isEmpty()
-  ],
-  userController.signup
-);
+// router.put(
+//   '/signup',
+//   [
+//     body('email')
+//       .isEmail()
+//       .withMessage('Please enter a valid email.')
+//       .custom((value, { req }) => {
+//         return User.findOne({ email: value }).then(userDoc => {
+//           if (userDoc) {
+//             return Promise.reject('E-Mail address already exists!');
+//           }
+//         });
+//       })
+//       .normalizeEmail(),
+//     body('password')
+//       .trim()
+//       .isLength({ min: 5 }),
+//     body('name')
+//       .trim()
+//       .not()
+//       .isEmpty()
+//   ],
+//   userController.signup
+// );
 
-router.post('/login', userController.login);
+// router.post('/login', userController.login);
 
-module.exports = router;
+// module.exports = router;
+
+
+
+                        // COMMENTED THIS FILE TO IMPLEMENT GRAPHQL //
