1a43094dd7a70bde44ff5c5b62ffcd833faa2c01
snadi043 <snadi043@uottawa.ca>
2025-05-28 15:56:29 -0400

    Chaining the frontend with the graphqlGetPostById query to implement the action when user clicks on the "view" option in the application.

--- Frontend/src/pages/Feed/SinglePost/SinglePost.js
+++ Frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -19,24 +19,41 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('http://localhost:8080/feeds/post/' + postId, {
+    const graphqlGetPostById = {
+      query: `{
+      getPostById(id: "${postId}")
+      {
+        title
+        content
+        iamgeUrl
+        creator {
+          name
+        }
+        createdAt
+        } 
+      }`
+    };
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
-        Authorization: 'Bearer ' + this.props.token
-      }
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(graphqlGetPostById)
     })
       .then(res => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch status');
-        }
         return res.json();
       })
       .then(resData => {
+        if(resData.errors){
+          throw new Error('Fetching Single Post failed.');
+        }
         this.setState({
-          title: resData.post.title,
-          author: resData.post.creator.name,
-          image: 'http://localhost:8080/' + resData.post.imageUrl,
-          date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
-          content: resData.post.content
+          title: resData.data.post.title,
+          author: resData.data.post.creator.name,
+          image: 'http://localhost:8080/' + resData.data.post.imageUrl,
+          date: new Date(resData.data.post.createdAt).toLocaleDateString('en-US'),
+          content: resData.data.post.content
         });
       })
       .catch(err => {
