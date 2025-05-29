4cef72b75233cd8c894d390bb7ba464bfd5ed8bf
snadi043 <snadi043@uottawa.ca>
2025-05-29 13:02:06 -0400

    .

--- Frontend/src/pages/Feed/Feed.js
+++ Frontend/src/pages/Feed/Feed.js
@@ -335,10 +335,6 @@ class Feed extends Component {
       .then(resData => {
         console.log(resData);
         this.loadPosts();
-        // this.setState(prevState => {
-        //   const updatedPosts = prevState.posts.filter(p => p._id !== postId);
-        //   return { posts: updatedPosts, postsLoading: false };
-        // });
       })
       .catch(err => {
         console.log(err);
